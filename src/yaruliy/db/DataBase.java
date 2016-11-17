ecc6de5fb90e614c02dbffd5eb64b9030edd3f7b
Yaroslav <l.yaruk1993@gmail.com>
2016-11-17 14:48:27 +0200

    second commit

--- src/yaruliy/db/DataBase.java
+++ src/yaruliy/db/DataBase.java
@@ -1,5 +1,21 @@
 package yaruliy.db;
+import java.util.ArrayList;
+import java.util.Properties;
 
 public class DataBase {
+    short partitionCount;
+    short replicationCount;
+    private ArrayList<Node> nodes;
+    Properties properties = new Properties();
 
+    public DataBase(){
+        initProperties();
+        this.partitionCount = Short.parseShort(properties.getProperty("partitionCount"));
+        this.replicationCount = Short.parseShort(properties.getProperty("replicationCount"));
+        nodes = new ArrayList<>(Integer.parseInt(properties.getProperty("replicationCount")));
+    }
+
+    private void initProperties() {
+
+    }
 }
