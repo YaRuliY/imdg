5bcb84b698c2e76282e45ad64a4ae853f1e26d26
Yaroslav <l.yaruk1993@gmail.com>
2016-11-20 01:06:09 +0200

    db structure

--- src/yaruliy/machine/MainFrame.java
+++ src/yaruliy/machine/MainFrame.java
@@ -7,20 +7,24 @@ public class MainFrame {
         /*IMDGObject object = new User(1, "name", "sername", 5);
         System.out.println("All Dependency:");
         System.out.println(object.getObjectDependency().getAllDependencyInString());*/
-        BloomFilter bf = new BloomFilter(1000, 10);
-        
+        BloomFilter bf = new BloomFilter(5000, 10);
+
         for (int i = 0; i < 100; i++){
             bf.add(i*100);
-            System.out.println("Adding for " + i*100);
         }
-        /*System.out.println();
 
-        for (int i = 0; i < 150; i++){
-            System.out.println("Query for " + i * 100 + ": " + bf.mightContain(i * 100));
-        }*/
-
+        /*Logger.log("BEGIN: ", false);
         for (int i = 0; i < 150; i++){
             Logger.log("Query for " + i * 100 + ": " + bf.mightContain(i * 100));
+        }*/
+
+        int count = 0;
+        for (int i = 100; i < 150; i++){
+            if (bf.mightContain(i * 100)){
+                count++;
+            }
         }
+        System.out.println("FALSE_POSITIVE count:" + count);
+        //Logger.log("FALSE_POSITIVE count:" + count);
     }
 }
--- src/yaruliy/util/Logger.java
+++ src/yaruliy/util/Logger.java
@@ -8,8 +8,15 @@ import java.io.IOException;
 public class Logger {
     private Logger(){}
 
+    public static void log(String string, boolean append){
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("main.log", append), "utf-8"))) {
+            writer.write(string + "\n");
+        }
+        catch (IOException e){ e.printStackTrace(); }
+    }
+
     public static void log(String string){
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("log.txt", true), "utf-8"))) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("main.log", true), "utf-8"))) {
             writer.write(string + "\n");
         }
         catch (IOException e){ e.printStackTrace(); }
