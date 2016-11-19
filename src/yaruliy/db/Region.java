a7b12fc274321ab81c6a3e7af754d4e20d9dc7e2
Yaroslav <l.yaruk1993@gmail.com>
2016-11-19 19:47:19 +0200

    m

--- src/yaruliy/db/Region.java
+++ src/yaruliy/db/Region.java
@@ -1,24 +1,5 @@
 package yaruliy.db;
-import java.util.ArrayList;
 
-class Region {
-    private short partitionCount;
-    private short replicationCount;
-    private ArrayList<Node> nodes;
+public interface Region {
 
-
-    public Region(){
-    }
-
-    public short getPartitionCount() {
-        return partitionCount;
-    }
-
-    public short getReplicationCount() {
-        return replicationCount;
-    }
-
-    public ArrayList<Node> getNodes() {
-        return nodes;
-    }
 }
