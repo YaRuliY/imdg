11e161c60d8a03775640b01f775d13efdd1f09e4
Yaroslav <l.yaruk1993@gmail.com>
2016-11-20 01:45:54 +0200

    db structure

--- src/yaruliy/machine/MainFrame.java
+++ src/yaruliy/machine/MainFrame.java
@@ -4,9 +4,6 @@ import yaruliy.util.Logger;
 
 public class MainFrame {
     public static void main(String a[]){
-        /*IMDGObject object = new User(1, "name", "sername", 5);
-        System.out.println("All Dependency:");
-        System.out.println(object.getObjectDependency().getAllDependencyInString());*/
         BloomFilter bf = new BloomFilter(5000, 10);
 
         for (int i = 0; i < 100; i++){
@@ -25,6 +22,5 @@ public class MainFrame {
             }
         }
         System.out.println("FALSE_POSITIVE count:" + count);
-        //Logger.log("FALSE_POSITIVE count:" + count);
     }
 }
